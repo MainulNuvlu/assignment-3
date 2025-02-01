be47002798b8461cee6866f2081e1cafde728d61
MainulNuvlu <mainulnuvlu.alias@gmail.com>
2025-02-01 13:30:45 +0600

    animation to text in banner is done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,20 @@ module.exports = {
     theme: {
         extend: {},
     },
-    plugins: [],
-}
+    // plugins: [],
+    // theme: {
+    //     extend: {
+    //         keyframes: {
+    //             gradient: {
+    //                 "0%": {
+    //                     backgroundPosition: "0% 50%"
+    //                 },
+    //                 "100%": { backgroundPosition: "100% 50%" },
+    //             },
+    //         },
+    //         animation: {
+    //             gradient: "gradient 6s linear infinite",
+    //         },
+    //     },
+    // },
+},
